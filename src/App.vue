b6b2edbf472df46dddeeda37e9024e1f11777721
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2020-02-12 13:16:49 -0700

    working app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class='container'>
+    <h1> First Things First </h1>
+
+    <md-card class="todoBox">
+      <md-field>
+        <md-input v-model="currentTodo" 
+        @keydown.enter="addTodo()" 
+        placeholder="Add an item to your todo-list!">
+        </md-input>
+      </md-field>
+      <div class="todos">
+        <li v-for="(todo, index) in todos" :key="todo.id">
+          <input class ='completeButton' type='checkbox' v-model="todo.completed">
+              
+              <span
+              class="todo-item-label"
+              :class='{completed: todo.completed}' 
+              @dblclick='editTodo(todo)' 
+              v-if="!todo.edit">
+                {{todo.label}}
+              </span>
+              
+              <md-input v-else class="todo-item-edit" 
+              type="text" v-model='todo.label'
+              @keyup.enter="completedEdit(todo)"> 
+              </md-input>
+          <md-raised class='closeButton' @click="removeTodo(index)">X</md-raised>
+        </li>
+      </div>
+    </md-card>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      currentTodo: '',
+      editedTodo: null
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length, 
+        label: this.currentTodo, 
+        completed: false,
+        edit: false
+        });
+      this.currentTodo = '';
+      },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+       },
+    editTodo(todo) {
+     todo.edit = true;
+    },
+    completedEdit(todo) {
+      todo.edit = false;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+li {
+  list-style: none;
 }
+.container {
+  margin: auto;
+  text-align: center;
+  width: 50%;
+}
+.completed {
+  text-decoration: line-through;
+}
+.todoBox {
+  font-size: 1.5em;
+  padding: 0px 5px 2px 5px;
+ 
+}
+.cardBox {
+padding: 5px;
+}
+.closeButton {
+  float: left;
+}
+.closeButton:hover {
+  cursor: pointer;
+  transition: 0.2s ease;
+    color: red
+}
+.completeButton {
+  float: right;
+}
+.todos {
+  margin: 10px;
+}
+.md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
 </style>
